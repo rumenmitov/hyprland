c7f1d146c83c7ca8c837bebbe3d18ec8284cfef3
Rumen <rumenmitov@protonmail.com>
2024-03-31 10:21:32 +0200

    changing date notification to time

--- scripts/info.sh
+++ scripts/info.sh
@@ -1,1 +1,1 @@
-dunstify "INFO" "🕑 <i>$(date +%d-%m-%y)</i>  🔋<b>$(cat /sys/class/power_supply/BAT0/capacity)%</b>    " -I "/home/rumen/.config/hypr/assets/icons/bell.png"
+dunstify "INFO" "🕑 <i>$(date +%H:%M)</i>  🔋<b>$(cat /sys/class/power_supply/BAT0/capacity)%</b>" -I "/home/rumen/.config/hypr/assets/icons/bell.png"
